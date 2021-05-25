b04d26d91d833961eb9feaed9780701ead8f3a99
Bradley <bmcguigandev@outlook.com>
2021-05-26 00:37:59 +0100

    Allow adding of new books

--- scripts/main.js
+++ scripts/main.js
@@ -8,24 +8,46 @@ function Book(title, author, pages, read) {
     }
 }
 
+const tableBody = document.querySelector('.table-body');
+
 function addBookToLibrary(title, author, pages, read) {
-    books.push(new Book(title, author, pages, read))
+    const newBook = new Book(title, author, pages, read)
+    books.push(newBook);
+    addBookToTable(newBook);
+}
+
+function addBookToTable({ title, author, pages, read }) {
+    tableBody.insertAdjacentHTML('beforeend',
+                                `<tr>
+                                    <td>${title}</td>
+                                    <td>${author}</td>
+                                    <td>${pages}</td>
+                                    <td>${read}</td>
+                                </tr>`
+                                );
 }
 
 const books = [];
 
-addBookToLibrary('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', '250', true);
-addBookToLibrary('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', '300', false);
+addBookToLibrary('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 250, true);
+addBookToLibrary('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 300, false);
 
-const tableBody = document.querySelector('.table-body');
+const newBookButton = document.querySelector('.new-book-button');
+const addBookButton = document.querySelector('.add-book-button');
+const newBookForm = document.querySelector('.new-book-form');
+const newBookFormTitle = document.querySelector('#title');
+const newBookFormAuthor = document.querySelector('#author');
+const newBookFormPages = document.querySelector('#pages');
+const newBookFormRead = document.querySelectorAll('.read');
 
-books.forEach(book => {
-    tableBody.insertAdjacentHTML('beforeend',
-                                `<tr>
-                                    <td>${book.title}</td>
-                                    <td>${book.author}</td>
-                                    <td>${book.pages}</td>
-                                    <td>${book.read}</td>
-                                </tr>`
-                                )
-})
+newBookButton.addEventListener('click', () => newBookForm.classList.toggle('invisible'));
+addBookButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addBookToLibrary(
+        title.value,
+        author.value,
+        Number(pages.value),
+        newBookFormRead[0].checked ? Boolean(newBookFormRead[0].value) : !Boolean(newBookFormRead[0].value)
+    );
+});
